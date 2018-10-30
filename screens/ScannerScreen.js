cd732066790871f74f4c14a36ef6de4de86ab852
katros <kasia.roszak@gmail.com>
2018-10-30 22:14:17 +0100

    scanner turned off when msg displayed

--- screens/ScannerScreen.js
+++ screens/ScannerScreen.js
@@ -1,31 +1,3 @@
-// import React from 'react';
-// import { ScrollView, StyleSheet } from 'react-native';
-// import { ExpoLinksView } from '@expo/samples';
-
-// export default class LinksScreen extends React.Component {
-//     static navigationOptions = {
-//         title: 'Links'
-//     };
-
-//     render() {
-//         return (
-//             <ScrollView style={styles.container}>
-//                 {/* Go ahead and delete ExpoLinksView and replace it with your
-//            * content, we just wanted to provide you with some helpful links */}
-//                 <ExpoLinksView />
-//             </ScrollView>
-//         );
-//     }
-// }
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         paddingTop: 15,
-//         backgroundColor: '#fff'
-//     }
-// });
-
 import React from 'react';
 import { StyleSheet, Text, View, Alert } from 'react-native';
 import { BarCodeScanner, Permissions } from 'expo';
@@ -33,12 +5,16 @@ import { BarCodeScanner, Permissions } from 'expo';
 export default class BarcodeScanner extends React.Component {
     static navigationOptions = {
         title: 'Scan QR Code'
-        // header: null
     };
 
-    state = {
-        hasCameraPermission: null
-    };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hasCameraPermission: null,
+            handleBarCodeScanned: this.handleBarCodeScanned
+        };
+    }
 
     async componentWillMount() {
         const { status } = await Permissions.askAsync(Permissions.CAMERA);
@@ -46,7 +22,7 @@ export default class BarcodeScanner extends React.Component {
     }
 
     render() {
-        const { hasCameraPermission } = this.state;
+        const { hasCameraPermission, handleBarCodeScanned } = this.state;
 
         if (hasCameraPermission === null) {
             return <Text>Requesting for camera permission</Text>;
@@ -57,31 +33,38 @@ export default class BarcodeScanner extends React.Component {
         return (
             <View style={{ flex: 1 }}>
                 <BarCodeScanner
-                    onBarCodeScanned={this.handleBarCodeScanned}
+                    onBarCodeScanned={handleBarCodeScanned}
+                    barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
                     style={StyleSheet.absoluteFill}
                 />
             </View>
         );
     }
 
-    handleBarCodeScanned = ({ type, data }) => {
+    handleBarCodeScanned = ({ data }) => {
+        let prevState = this.state.handleBarCodeScanned;
+
         fetch('http://api.ethplorer.io/getTxInfo/' + data + '?apiKey=freekey')
             .then(response => response.json())
             .then(data => {
+                this.setState({ handleBarCodeScanned: undefined });
+
                 let message = data.input;
                 let convertedMsg = this.hexToString(message);
-                // Alert.alert('This is the message:' + convertedMsg);
+
                 Alert.alert('Message:', convertedMsg, [
-                    { text: 'CLOSE', onPress: () => console.log('CLOSE Pressed') }
+                    { text: 'CLOSE', onPress: () => this.setState({ handleBarCodeScanned: prevState }) }
                 ]);
             });
     };
 
     hexToString = hex => {
-        let str = '';
-        for (let i = 6; i < hex.length; i += 2) {
-            str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+        if (hex !== undefined) {
+            let str = '';
+            for (let i = 6; i < hex.length; i += 2) {
+                str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+            }
+            return str;
         }
-        return str;
     };
 }
